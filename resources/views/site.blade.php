4cec12447d1bc7e9aab99e03d0c0e5e131408cfd
Flavio Lima <flaviolima@Air-de-Flavio.lan>
2024-09-02 02:09:31 -0300

    Creat db ativos and timeframes

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>Negociação de Forex, Ações, ETFs e Opções</title>
+
+    <link rel="shortcut icon" type="image/jpg" href="{{ asset('img/icon todo branco.png') }}"/>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
@@ -76,7 +78,7 @@
                         <div class="left-header-tabcontent">
                             <a href="#" class="title-tab">
                                 <span>42</span>
-                                Pares Forex
+                                Ativos dispoíveis
     
                                 <i class="fa-solid fa-chevron-right"></i>
                             </a>
